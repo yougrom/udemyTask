a8cb971d0a890be30b03b43f92222f5456078b01
Herr Gromov <youriy.gromov@gmail.com>
2022-06-24 14:57:48 +0200

    finish 4 5 6 tasks

--- 04-strings-concatenation/solution.js
+++ 04-strings-concatenation/solution.js
@@ -11,9 +11,9 @@
  * 3. Выведите значение последней переменной в консоль
  */
 
-const myName = 'Bogdan'
-const mySurname = 'Stashchuk'
-const myProfession = 'Instructor'
+const myName = 'Yuriy'
+const mySurname = 'Gromov'
+const myProfession = 'Web Developer'
 
 // OPTION 1
 const personInfo1 =
@@ -25,3 +25,27 @@ console.log(personInfo1)
 const personInfo2 = `My name is ${myName} ${mySurname} and I am ${myProfession}`
 
 console.log(personInfo2)
+
+//Шаблонные строки
+
+const str = `Я первая строка.
+А я вторая строка.
+
+Ну а я третья, но надо мной и подо мной пустая строка и перенос.
+
+Вообще так можно и стихотворение написать.
+`;
+console.log(str)]
+
+//Многострочные литералы
+console.log(`string text line 1
+string text line 2`);
+// "string text line 1
+//  string text line 2"
+
+const a = 5;
+const b = 10;
+console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`);
+// "Fifteen is 15 and not 20."
+
+//Вложенные шаблоны
--- 06-data-types/finish.js
+++ 06-data-types/finish.js
@@ -12,3 +12,19 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const perString = 'string';
+const perNumber = 108;
+const perBoolen = true;
+const perNull = null
+let perUndefined
+const perObject = {};
+const perMassiv = [];
+
+console.log(typeof perString)
+console.log(typeof perNumber);
+console.log(typeof perBoolen);
+console.log(typeof perNull);
+console.log(typeof perUndefined);
+console.log(typeof perObject);
+console.log(typeof perMassiv);
